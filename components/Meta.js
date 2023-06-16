091b6f58efb530bff95cc043c49655032481aed7
Paul Hayes <fofr@users.noreply.github.com>
2023-06-16 14:36:33 +0100

    Add og image

--- components/Meta.js
+++ components/Meta.js
@@ -3,8 +3,16 @@ import { Fragment } from 'react';
 const Meta = () => {
   return (
     <Fragment>
-      <meta property="og:title" content="Waverformer" />
-      <meta property="og:description" content="Text to waveform video using MusicGen" />
+      <meta property="og:title" content="Waveformer" />
+      <meta property="og:description" content="Text to music using MusicGen" />
+      <meta property="og:image" content="https://waveformer.replicate.dev/og.png" />
+      <meta property="og:url" content="https://waveformer.replicate.dev" />
+      <meta property="og:type" content="website" />
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://waveformer.replicate.dev" />
+      <meta property="twitter:title" content="Waveformer" />
+      <meta property="twitter:description" content="Text to music using MusicGen" />
+      <meta property="twitter:image" content="https://waveformer.replicate.dev/og.png" />
     </Fragment>
   );
 };
