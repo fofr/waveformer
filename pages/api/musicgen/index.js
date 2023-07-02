3ce1a04c982beac535d68d758cbd6d66dc5a4f45
Paul Hayes <fofr@users.noreply.github.com>
2023-07-02 17:45:15 +0100

    Use latest version of model

--- pages/api/musicgen/index.js
+++ pages/api/musicgen/index.js
@@ -1,7 +1,7 @@
 import createApiHandler from "../api-handler";
 
 const handler = createApiHandler(
-  "2c56751d7f8642caf84cc64ec583c70087ac5ed90b27ca0005d00ff1be505417",
+  "7a76a8258b23fae65c5a22debb8841d1d7e816b75c2f24218cd2bd8573787906",
   (body) => {
     let duration = parseInt(body.duration) || 8;
     duration = Math.min(Math.max(duration, 1), 30)
